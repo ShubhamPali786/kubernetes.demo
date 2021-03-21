dd46f32783cea4fb9ac7ad33129ad86d5eb7632e
ShubhamPali786 <shubhampali1993@gmail.com>
2021-03-21 22:53:08 +0530

    Config Map read changes

--- Kubernetes.ConfigMaps.Demo/Program.cs
+++ Kubernetes.ConfigMaps.Demo/Program.cs
@@ -18,6 +18,10 @@ namespace Kubernetes.ConfigMaps.Demo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(c =>
+                {
+                    c.AddJsonFile("config/appsettings.json", optional: true, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
